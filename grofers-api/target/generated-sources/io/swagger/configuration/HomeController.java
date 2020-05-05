e340eae221582b38115a7b25ec2c0130f760b40e
varunbansal644 <varunbansal644@gmail.com>
2020-05-05 15:11:04 +0530

    Target autogenerated

--- grofers-api/target/generated-sources/io/swagger/configuration/HomeController.java
+++ grofers-api/target/generated-sources/io/swagger/configuration/HomeController.java
@@ -3,9 +3,7 @@ package io.swagger.configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * Home redirection to swagger api documentation 
- */
+
 @Controller
 public class HomeController {
     @RequestMapping(value = "/")
